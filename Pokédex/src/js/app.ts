8faa1e219605fddb7da6e66214298593110f97c6
Alex Arden <alexcander444@gmail.com>
2022-06-30 00:36:37 +0300

    Added search and render pokemon feature

--- Pokédex/src/js/app.ts
+++ Pokédex/src/js/app.ts
@@ -5,28 +5,36 @@ import Pokemon from './Pokemon'
 let container = document.querySelector('.container') as HTMLDivElement
 
 let searchButton = document.querySelector('.search-button') as HTMLButtonElement
-
-let url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
-
-async function getPokemonList(){
-  let pokemonList = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9')  
-  let listData = await pokemonList.json();    
-  
-  listData.results.forEach((item: { url: string; })  => {
-    fetchPokemon(item.url)
-  })
-
-}
-
-
-async function fetchPokemon(url: string) {
-  let pokemons = await fetch(url); 
-  let data = await pokemons.json();  
-    
-  let pokemon = new Pokemon(container, data) 
-    
-}
-
-getPokemonList();
-// fetchPokemon(url);  
+let input = document.querySelector('.input') as HTMLInputElement
+searchButton.addEventListener('click', () => {
  
+  let list = getPokemonList('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151');
+  list.then(value => {
+    value.results.forEach((item: { name: string; url :string}) => {
+      if(item.name === input.value){
+        renderPokemon(item.url);
+        input.value = ''; 
+      }
+    })
+  })
+}) 
+
+
+
+let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10';
+
+async function getPokemonList(url: string){ 
+  let pokemonList = await fetch(url)  
+  let listData = await pokemonList.json();    
+  return listData
+}
+
+async function renderPokemon(url: string) {
+  let pokemons = await fetch(url);
+  let data = await pokemons.json();
+  let pokemon = new Pokemon(container, data);
+}
+
+let test = getPokemonList(url); 
+test.then(value => console.log(value.results))    
+
