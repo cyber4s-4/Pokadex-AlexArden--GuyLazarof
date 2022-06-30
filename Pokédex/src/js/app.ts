dcfbe1bd651f5da654dabf43e8617ab873b6be01
Guy Lazarof <102980363+Guy-Lazarof@users.noreply.github.com>
2022-06-30 12:34:00 +0300

    not found massage+ show all pokemons

--- Pokédex/src/js/app.ts
+++ Pokédex/src/js/app.ts
@@ -1,33 +1,46 @@
-//https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10
+import Pokemon from './Pokemon';
 
+//https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10
 //TODO: make pokemons button render all pokemons.
 //TODO: when searching pokemon render only searched pokemon.
 
-import Pokemon from './Pokemon'
+let container = document.querySelector(".container") as HTMLDivElement;
 
-let container = document.querySelector('.container') as HTMLDivElement
-
-let searchButton = document.querySelector('.search-button') as HTMLButtonElement
-let input = document.querySelector('.input') as HTMLInputElement
-searchButton.addEventListener('click', () => {
- 
-  let list = getPokemonList('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151');
-  list.then(value => {
-    value.results.forEach((item: { name: string; url :string}) => {
-      if(item.name === input.value){
+let searchButton = document.querySelector(
+  ".search-button"
+) as HTMLButtonElement;
+let input = document.querySelector(".input") as HTMLInputElement;
+searchButton.addEventListener("click", () => {
+  let list = getPokemonList(
+    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151"
+  );
+  list.then((value) => {
+    let check = false;
+    value.results.forEach((item: { name: string; url: string }) => {
+      if (item.name === input.value) {
+        container.innerHTML = "";
         renderPokemon(item.url);
-        input.value = ''; 
+        input.value = "";
+        check = true;
       }
-    })
-  })
-}) 
+    });
+    if (check === false) {
+      container.innerHTML = "";
+      let notExistsPokemon = document.createElement("div");
+      notExistsPokemon.className = "notFound";
+      notExistsPokemon.innerHTML = `<u>${input.value}</u> &nbsp is not a pokemon`;
+      container.appendChild(notExistsPokemon);
+      input.value = "";
+    }
+  });
+});
 
-let url = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10';
+let url = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10";
 
-async function getPokemonList(url: string){ 
-  let pokemonList = await fetch(url)  
-  let listData = await pokemonList.json();    
-  return listData
+async function getPokemonList(url: string) {
+  let pokemonList = await fetch(url);
+  let listData = await pokemonList.json();
+  return listData;
 }
 
 async function renderPokemon(url: string) {
@@ -35,13 +48,18 @@ async function renderPokemon(url: string) {
   let data = await pokemons.json();
   let pokemon = new Pokemon(container, data);
 }
+let allPokemons = document.querySelector(".allPokemons") as HTMLDivElement;
+allPokemons.addEventListener("click", () => {
+  container.innerHTML = "";
+  let pokemonList = getPokemonList(url);
+  pokemonList.then((value) => console.log(value.results));
 
-// let test = getPokemonList(url); 
-// test.then(value => console.log(value.results))    
-
-// let list = getPokemonList('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151');
-// list.then(value => {
-//   value.results.forEach((item: {url: string; name: string }) => {
-//     renderPokemon(item.url) 
-//   })
-// })    
+  let list = getPokemonList(
+    "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151"
+  );
+  list.then((value) => {
+    value.results.forEach((item: { url: string; name: string }) => {
+      renderPokemon(item.url);
+    });
+  });
+});
