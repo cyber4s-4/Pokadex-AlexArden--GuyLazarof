f4ae8e6393b73c06db7909ac418b7513967a2c1f
Guy Lazarof <102980363+Guy-Lazarof@users.noreply.github.com>
2022-07-01 15:44:56 +0300

    add img types

--- Pokédex/src/js/Pokemon.ts
+++ Pokédex/src/js/Pokemon.ts
@@ -9,12 +9,46 @@ export default class Pokemon {
   render(parent: HTMLDivElement, pokemonData: pokemon) {
     let pokemonDiv = document.createElement("div");
     pokemonDiv.classList.add("pokemon");
+    let type = pokemonData.types[0].type.name;
+    let typeImg: string;
+    if (type === `bug`) {
+      typeImg = "/img/ladybug.png";
+    } else if (type === `water`) {
+      typeImg = `/img/water-drop.png`;
+    } else if (type === `poison`) {
+      typeImg = `/img/virus.png`;
+    } else if (type === `electric`) {
+      typeImg = `/img/lightning.png`;
+    } else if (type === `fire`) {
+      typeImg = `/img/fire.png`;
+    } else if (type === `rock`) {
+      typeImg = `/img/rock.png`;
+    } else if (type === `ice`) {
+      typeImg = `/img/snowflake.png`;
+    } else if (type === `grass`) {
+      typeImg = `/img/grass.png`;
+    } else if (type === `fighting`) {
+      typeImg = `/img/fight.png`;
+    } else if (type === `fairy`) {
+      typeImg = `/img/fairy.png`;
+    } else if (type === `dragon`) {
+      typeImg = `/img/dragon.png`;
+    } else if (type === `normal`) {
+      typeImg = `/img/popular.png`;
+    } else if (type === `psychic`) {
+      typeImg = `/img/crystal-ball.png`;
+    } else if (type === `ground`) {
+      typeImg = `/img/dunes.png`;
+    }
+    // if ( type.innerHTML ===ground) {
+    //   type
+    // }
     pokemonDiv.innerHTML = `
      <img src="${pokemonData.sprites.front_default}">
      <div class="name">${pokemonData.name}</div>
      <div class="heigth">Height: ${pokemonData.height / 10} meter</div>
      <div class="weigth">Weigth: ${pokemonData.weight / 10} kg</div>
-     <div class="type">${pokemonData.types[0].type.name}</div>
+     <div class="type"><img src="${typeImg}"/>${type} </div>
     `;
     parent.appendChild(pokemonDiv);
   }
